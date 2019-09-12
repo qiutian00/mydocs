455d232e2830795cdffeecb2d9c0cbd32c3c32c3
qiutian00 <1476102147@qq.com>
2019-09-12 15:37:07 +0800

    docs: update readme.md && config.js

--- teadocs.config.js
+++ teadocs.config.js
@@ -16,7 +16,7 @@ module.exports = {
         headHtml: "",
         footHtml: "",
         isMinify: true,
-        rootPath: "/"
+        rootPath: "/mydocs/"
     },
     nav: {
         tree: "./tree"
